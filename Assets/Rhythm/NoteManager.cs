e6444c928bcce0df291d4717b8fe38453bc981ce
Seung Ho Jang <shjang1992@hotmail.com>
2015-01-15 02:06:25 +0900

    Working sample initialized

---
+++ Assets/Rhythm/NoteManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class NoteManager : MonoBehaviour
+{
+    static public NoteManager Instance = null;
+
+    private List<KeyValuePair<Note, NoteCriterion>> hitList = new List<KeyValuePair<Note, NoteCriterion>>();
+
+    // Use this for initialization
+    void Start()
+    {
+        // Temp singleton
+        Instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (var pair in hitList)
+        {
+            var note = pair.Key as Note;
+            var criterion = pair.Value as NoteCriterion;
+
+            Destroy(note.gameObject);
+        }
+
+        hitList.Clear();
+    }
+
+    public void AddHitGroup(Note note, NoteCriterion criterion)
+    {
+        hitList.Add(new KeyValuePair<Note, NoteCriterion>(note, criterion));
+    }
+}
