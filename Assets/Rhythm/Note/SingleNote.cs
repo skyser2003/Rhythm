8305c3c22a6728d901ba64ba457f2916c0c1b670
Seung Ho Jang <shjang1992@hotmail.com>
2015-01-17 02:35:17 +0900

    Sample state

--- Assets/Rhythm/Note/SingleNote.cs
+++ Assets/Rhythm/Note/SingleNote.cs
@@ -1,20 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SingleNote : Note {
+public class SingleNote : Note
+{
 
     SingleNote()
     {
         type = NOTE_TYPE.SINGLE_NOTE;
     }
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //gameObject.rigidbody.velocity = new Vector3(0, 0, -10);
+    }
 }
