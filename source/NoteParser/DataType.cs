94eb313be0eaa10dc1547f4b55d4c5301a1d5107
Seung Ho Jang <shjang1992@hotmail.com>
2015-02-01 18:19:47 +0900

    Data class seperated

---
+++ source/NoteParser/DataType.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NoteParser
+{
+    public class File
+    {
+        public Header header;
+        public Node[] nodes;
+    }
+
+    public class Header
+    {
+        public string songName;
+        public string author;
+        public int difficulty;
+    }
+
+    public class Node
+    {
+        public double speed;
+        public Notes notes;
+    }
+
+    public class Notes
+    {
+        public SingleNote[] singleNotes;
+        public LongNote[] longNotes;
+    }
+
+    public class Note
+    {
+        public string type;
+    }
+
+    public class SingleNote : Note
+    {
+        public double time;
+        public Vector2[] coords;
+    }
+
+    public class LongNote : Note
+    {
+        public double beginTime;
+        public double endTime;
+    }
+
+    public class Vector2
+    {
+        public double x;
+        public double y;
+    }
+}
--- source/NoteParser/Document.cs
+++ source/NoteParser/Document.cs
@@ -8,54 +8,6 @@ namespace NoteParser
 {
     public class Document
     {
-        public class File
-        {
-            public Header header;
-            public Node[] nodes;
-        }
-
-        public class Header
-        {
-            public string songName;
-            public string author;
-            public int difficulty;
-        }
-
-        public class Node
-        {
-            public double speed;
-            public Notes notes;
-        }
-
-        public class Notes
-        {
-            public SingleNote[] singleNotes;
-            public LongNote[] longNotes;
-        }
-
-        public class Note
-        {
-            public string type;
-        }
-
-        public class SingleNote : Note
-        {
-            public double time;
-            public Vector2[] coords;
-        }
-
-        public class LongNote : Note
-        {
-            public double beginTime;
-            public double endTime;
-        }
-
-        public class Vector2
-        {
-            public double x;
-            public double y;
-        }
-
         public void Parse(string content)
         {
             var file = JsonMapper.ToObject<File>(content);
