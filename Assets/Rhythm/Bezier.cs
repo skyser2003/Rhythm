cf3161adda40cb5491cbabe8f55217519d60fd32
Seung Ho Jang <shjang1992@hotmail.com>
2015-02-06 01:25:41 +0900

    Wrong math equation fixed

--- Assets/Rhythm/Bezier.cs
+++ Assets/Rhythm/Bezier.cs
@@ -6,6 +6,7 @@ using System.Text;
 class Bezier
 {
     private NoteParser.Vec3[] coords;
+    private double[] coefficients;
 
     private int Binomial(int n, int i)
     {
@@ -17,15 +18,12 @@ class Bezier
         int dividend = 1;
         int divisor = 1;
 
-        for (int j = 1; j <= n; ++j)
+        for (int j = i; j <= n; ++j)
         {
             dividend *= j;
         }
+
         for (int j = 1; j < i; ++j)
-        {
-            divisor *= j;
-        }
-        for (int j = 1; j < n - i; ++j)
         {
             divisor *= j;
         }
@@ -36,31 +34,33 @@ class Bezier
     public void Init(NoteParser.Vec3[] coords)
     {
         this.coords = coords;
-    }
-
-    public NoteParser.Vec3 GetPosition(float t)
-    {
-        t = Math.Min(1.0f, t);
-        t = Math.Max(0.0f, t);
-
-        float rt = 1 - t;
 
         int n = coords.Length;
-
-        var ret = new NoteParser.Vec3();
+        coefficients = new double[n];
 
         for (int i = 0; i < n; ++i)
         {
-            double coefficient = Binomial(n, i);
-            for (int j = 0; j < n - i; ++j)
-            {
-                coefficient *= rt;
-            }
-            for (int j = 0; j < i; ++j)
-            {
-                coefficient *= t;
-            }
+            coefficients[i] = Binomial(n - 1, i);
+        }
+    }
 
+    public NoteParser.Vec3 GetPosition(float ft)
+    {
+        double t = (double)ft;
+
+        t = Math.Min(1.0f, t);
+        t = Math.Max(0.0f, t);
+
+        double rt = 1 - t;
+
+        int n = coords.Length - 1;
+
+        var ret = new NoteParser.Vec3();
+
+        for (int i = 0; i <= n; ++i)
+        {
+            double coefficient = coefficients[i];
+            coefficient = coefficient * Math.Pow(rt, n - i) * Math.Pow(t, i);
             ret += coefficient * coords[i];
         }
 
